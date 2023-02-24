eda6a65ae63429043483c8a7a1d84358d35c188c
EdvaldoFagundesmj <edvaldofagundesmj@gmail.com>
2023-02-24 09:32:14 -0300

    operadores relacionais comparacaoavancada 01

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,26 +1,28 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        //classe Operadores.java
-int numero1 = 1;
-int numero2 = 2;
 
-if(numero1 > numero2)
-	System.out.print("Numero 1 maior que numero 2");
+// ComparacaoAvancada.java
 
-if(numero1 < numero2)
-	System.out.print("Numero 1 menor que numero 2");
-
-if(numero1 >= numero2)
-	System.out.print("Numero 1 maior ou igual que numero 2");
-
-if(numero1 <= numero2)
-	System.out.print("Numero 1 menor ou igual que numero 2 ");
-
-if(numero1 != numero2)
-	System.out.print("Numero 1 é diferente de numero 2");
-
+    String nome1 = "JAVA";
+    String nome2 = "JAVA";
     
+    System.out.println(nome1 == nome2); //true
+
+    String nome3 = new String("JAVA");
+    
+    System.out.println(nome1 == nome3); //false
+
+    String nome4 = nome3;
+
+    System.out.println(nome3 == nome4); //true
+    
+    //equals na parada
+    System.out.println(nome1.equals(nome2)); //??
+    System.out.println(nome2.equals(nome3)); //??
+    System.out.println(nome3.equals(nome4)); //??
+
+
 
     }
 }
